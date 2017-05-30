2ceab1fe11c7149bcacace196c4a90c13a8b83b8
Lance Harper <lancetharper@gmail.com>
2017-05-30 15:30:32 -0700

    Fix icons

--- src/demo/Brand.vue
+++ src/demo/Brand.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="container">
-    <cdr-heading :level="1" :modifier="['headline']">Brand.ai demo</cdr-heading>
+    <cdr-heading :level="1" :modifier="['spruce-dsp-80']">Brand.ai demo</cdr-heading>
     <cdr-a href="https://www.rei.com" :modifier="['standalone']">A standalone link</cdr-a>
     <p>
-      <cdr-icon url="/static/icon-sprite.svg#icon-rei-star-100"></cdr-icon>
-      <cdr-icon url="/static/icon-sprite.svg#icon-rei-star-100"></cdr-icon>
-      <cdr-icon url="/static/icon-sprite.svg#icon-rei-star-100"></cdr-icon>
-      <cdr-icon url="/static/icon-sprite.svg#icon-rei-star-100"></cdr-icon>
-      <cdr-icon url="/static/icon-sprite.svg#icon-rei-star-100"></cdr-icon>
+      <cdr-icon url="/static/rei-icons.svg#icon-rei-star-100"></cdr-icon>
+      <cdr-icon url="/static/rei-icons.svg#icon-rei-star-100"></cdr-icon>
+      <cdr-icon url="/static/rei-icons.svg#icon-rei-star-100"></cdr-icon>
+      <cdr-icon url="/static/rei-icons.svg#icon-rei-star-100"></cdr-icon>
+      <cdr-icon url="/static/rei-icons.svg#icon-rei-star-100"></cdr-icon>
     </p>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt eaque, dignissimos ipsum amet vero excepturi eum illum impedit, perferendis nobis adipisci doloribus voluptatibus distinctio nemo velit harum odit vel! Odio.</p>
     <cdr-button>Primary Button</cdr-button>
-    <div>
-      <cdr-icon url="/static/icon-sprite.svg#icon-rei-star-100"></cdr-icon>
-    </div>
   </div>
 </template>
 
